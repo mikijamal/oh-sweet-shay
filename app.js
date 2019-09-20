79fa6a95553f85b5b6a76b2e0c179dc5b14e8a53
mkjml <mkvlrm@gmail.com>
2019-09-20 12:01:48 -0700

    change main.html to index.html to run on github pages

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/main.html");
+  res.sendFile(__dirname + "/index.html");
 });
-
+app.get("/gallery", function(req, res){
+  res.sendFile(__dirname + "/gallery.html");
+});
 app.listen(process.env.PORT || 3000, function(req, res){
   console.log("Server is working on port 3000.")
 });
